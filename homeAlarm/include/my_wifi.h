fe56219f151e1eb03284a282f2482a6de82164c5
Michael-Q-Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-11-24 22:24:42 -0600

    Add WiFi header (credentials need to be edited when using)

---
+++ homeAlarm/include/my_wifi.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+#ifndef MY_WIFI_H_
+#define MY_WIFI_H_
+
+char const *SSID            = "mySSID" ;
+char const *PASS            = "myPass!" ;
+uint8_t const   WEB_PORT    = 80 ;
+
+#endif /* MY_WIFI_H_ */
