9c44b40c0c4c726d657815b01356ac02389eecdb
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-11-25 20:42:47 -0600

    Combine everything to just main.cpp and main.h and add a button feature

--- homeAlarm/include/main.h
+++ homeAlarm/include/main.h
@@ -5,8 +5,61 @@
 
 #define BAUD_RATE       115200
 
-extern void initWifi() ;
-extern void checkReedState() ;
-extern void IRAM_ATTR ISR_buttonPressed(void) ;
+/* Pins */
+gpio_num_t const REED_PIN   = GPIO_NUM_12 ;
+gpio_num_t const LED_PIN    = GPIO_NUM_13 ;
+gpio_num_t const BUTTON     = GPIO_NUM_0 ;
+
+bool wifi_enabled ;
+
+
+/* Constants */
+
+int const READ_DELAY        = 100 ;
+char const *SSID            = "Misnomer" ;
+char const *PASS            = "3mb3d3dSystems!" ;
+// uint8_t const   WEB_PORT    = 80 ;
+
+/* Enumerations */
+
+typedef enum {
+    REED_CLOSED = LOW,
+    REED_OPEN   = HIGH
+} Reed_State_t ;
+
+typedef enum {
+    LED_ON      = HIGH ,
+    LED_OFF     = LOW
+} LED_State_t ;
+
+typedef enum {                                                                              // Switches are active LOW
+    BUTTON_ON  = LOW,
+    BUTTON_OFF = HIGH    
+} Button_State_t ;
+
+/* Structures */
+
+typedef struct {
+    uint16_t const              DEBOUNCE    = 500 ;
+    uint8_t volatile            count ;
+    portMUX_TYPE                mux         = portMUX_INITIALIZER_UNLOCKED ;
+    uint32_t volatile           interruptCounter ;
+    uint32_t                    totalInterruptCounter ;
+} Button_t ;
+
+/* Typedef Variables */
+Button_t                        button ;
+
+
+
+
+
+// WiFi
+void initWifi() ;
+// Reed Switch
+void checkReedState() ;
+// Button
+void IRAM_ATTR ISR_buttonPressed(void) ;
+void checkButtonPress(void) ;
 
 #endif  /* MAIN_H_ */
--- homeAlarm/src/main.cpp
+++ homeAlarm/src/main.cpp
@@ -1,15 +1,72 @@
 #include "main.h"
-#include "reedSwitch.h"
+// #include "reedSwitch.h"
+// #include "button_arm.h"
 
-extern void readDigitalVal() ;
+#include <Wifi.h>
+#include <stdint.h>
 
 void setup() {
     Serial.begin(BAUD_RATE) ;
 
     pinMode(REED_PIN, INPUT) ;
     pinMode(LED_PIN, OUTPUT) ;
+
+    // wifi_enabled = false ;
+    // initWifi() ;
+
+    attachInterrupt(digitalPinToInterrupt(BUTTON), ISR_buttonPressed, RISING) ;
 }
 
 void loop() {
-    readDigitalVal() ;
+    if (button.interruptCounter > 0) {
+        portENTER_CRITICAL(&button.mux) ;
+        button.interruptCounter-- ;
+        portEXIT_CRITICAL(&button.mux) ;
+
+        // Interrupt handling code
+        button.totalInterruptCounter++ ;
+        Serial.printf("A button interrupt has occurred. Total Number: %u\n", button.totalInterruptCounter) ;
+        if ( (button.totalInterruptCounter) && (button.totalInterruptCounter % 2) ) {
+            // Enable wifi flag
+
+            // Enable reedSwitch checking
+            checkReedState() ;
+        }
+    }
+    #ifdef later
+    // Figure out a way to enable wifi once and only o
+    // Make it so that this is a wakeup from sleep
+    if (wifiEnable) {
+        initWifi() ;
+        wifiEnable = false ;
+    }
+    #endif
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* FUNCTION PROTOTYPES */
+
+void checkReedState() {
+    Reed_State_t    reedState ;
+    LED_State_t     ledState ;
+
+    reedState = (Reed_State_t)( digitalRead(REED_PIN) ) ;
+
+    if (reedState != REED_CLOSED) {                                             // LED turns on if reed isn't closed
+        ledState = LED_ON ;
+        digitalWrite(LED_PIN, ledState) ;
+    }
+    else {
+        ledState = LED_OFF ;
+        digitalWrite(LED_PIN, ledState) ;
+    }
+
+    delay(READ_DELAY) ;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void IRAM_ATTR ISR_buttonPressed(void) {
+    portENTER_CRITICAL_ISR(&button.mux) ;
+    button.interruptCounter++ ;
+    portEXIT_CRITICAL_ISR(&button.mux) ;
+}
+
