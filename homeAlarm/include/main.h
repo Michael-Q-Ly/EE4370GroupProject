f20df59a40d2ce2516cef9ea35881f0c52428486
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-11-28 00:08:36 -0600

    Add deep and light sleep modes
    The setup function does everything we need it to due to deep sleep
    function essentially resetting the device.
    The code simply waits for the user to arm the device with a button,
    enters light sleep and waits for someone to trip the alarm,
    then it connects to WiFi and enters deep sleep.
    
    We will now need to send an email with SMTP.
    Then we need to add some robustness to check if the door is left open.

--- homeAlarm/include/main.h
+++ homeAlarm/include/main.h
@@ -5,7 +5,8 @@
 
 #define BAUD_RATE       115200
 
-/* Pins */
+/* -------------------------------------------- Pins -------------------------------------------- */
+
 gpio_num_t const REED_PIN   = GPIO_NUM_12 ;
 gpio_num_t const LED_PIN    = GPIO_NUM_13 ;
 gpio_num_t const BUTTON     = GPIO_NUM_0 ;
@@ -13,18 +14,15 @@ gpio_num_t const BUTTON     = GPIO_NUM_0 ;
 bool wifi_enabled ;
 
 
-/* Constants */
+/* -------------------------------------------- Constants -------------------------------------------- */
 
-int const READ_DELAY        = 100 ;
-char const *SSID            = "myNetworkName" ;
-char const *PASS            = "myNetworkPassword" ;
+uint8_t const READ_DELAY        = 100 ;
+char const *SSID                = "HuisATT 2.4GHz" ;
+char const *PASS                = "coffeesluts" ;
+// char const *SSID            = "myNetworkName" ;
+// char const *PASS            = "myNetworkPassword" ;
 // uint8_t const   WEB_PORT    = 80 ;
-int const TRIP_DELAY        = 5000 ;
-
-/* Flags */
-
-bool wifiEnable ;
-
+uint8_t const MAX_WIFI_ATTEMPTS = 20 ;
 
 /* Enumerations */
 
@@ -33,7 +31,7 @@ typedef enum {
     REED_OPEN   = HIGH
 } Reed_State_t ;
 
-typedef enum {
+typedef enum {                                                                              // Double check if active high or low
     LED_ON      = HIGH ,
     LED_OFF     = LOW
 } LED_State_t ;
@@ -43,42 +41,45 @@ typedef enum {                                                                  
     BUTTON_OFF = HIGH    
 } Button_State_t ;
 
-/* Structures */
+/* -------------------------------------------- Structures -------------------------------------------- */
 
-typedef struct {
-    uint16_t const              DEBOUNCE    = 500 ;
-    uint8_t volatile            count ;
-    portMUX_TYPE                mux         = portMUX_INITIALIZER_UNLOCKED ;
-    uint32_t volatile           interruptCounter ;
-    uint32_t                    totalInterruptCounter ;
-} Button_t ;
+// typedef struct {
+//     uint16_t const              DEBOUNCE    = 500 ;
+//     uint8_t volatile            count ;
+//     portMUX_TYPE                mux         = portMUX_INITIALIZER_UNLOCKED ;
+//     uint32_t volatile           interruptCounter ;
+//     uint32_t                    totalInterruptCounter ;
+// } Button_t ;
 
-typedef struct {
-    uint16_t const              DEBOUNCE    = 500 ;
-    uint8_t volatile            count ;
-    portMUX_TYPE                mux         = portMUX_INITIALIZER_UNLOCKED ;
-    uint32_t volatile           interruptCounter ;
-    uint32_t                    totalInterruptCounter ;
-} Reed_t ;
+// typedef struct {
+//     uint16_t const              DEBOUNCE    = 500 ;
+//     uint8_t volatile            count ;
+//     portMUX_TYPE                mux         = portMUX_INITIALIZER_UNLOCKED ;
+//     uint32_t volatile           interruptCounter ;
+//     uint32_t                    totalInterruptCounter ;
+// } Reed_t ;
 
-/* Typedef Variables */
-Button_t                        button ;
-Reed_t                          reed ;
+/* -------------------------------------------- Typedef Variables -------------------------------------------- */
+// Button_t                        button ;
+// Reed_t                          reed ;
 LED_State_t                     ledState ;
 Reed_State_t                    reedState ;
 esp_sleep_wakeup_cause_t        wakeup_reason ;
 
 
+
+/* -------------------------------------------- Function Prototypes -------------------------------------------- */
 // WiFi
-void initWifi(void) ;
+void init_Wifi(void) ;
 // Reed Switch
-void checkReedState(void) ;
+// void checkReedState(void) ;
 // Interrupt handlers
-void IRAM_ATTR ISR_buttonPressed(void) ;
-void IRAM_ATTR ISR_reedOpen(void) ;
+// void IRAM_ATTR ISR_buttonPressed(void) ;
+// void IRAM_ATTR ISR_reedOpen(void) ;
 // Sleep modes
-void deepSleep(void) ;
-void lightSleep(void) ;
+void light_sleep_button(void) ;
+void light_sleep_switch(void) ;
+void deep_sleep(void) ;
 
 // void checkButtonPress(void) ;
 
--- homeAlarm/src/main.cpp
+++ homeAlarm/src/main.cpp
@@ -8,101 +8,69 @@
 void setup() {
     Serial.begin(BAUD_RATE) ;
 
-    pinMode(REED_PIN, INPUT) ;
-    pinMode(LED_PIN, OUTPUT) ;
+    pinMode(BUTTON,     INPUT_PULLUP) ;
+    pinMode(REED_PIN,   INPUT_PULLUP) ;
+    pinMode(LED_PIN,    OUTPUT) ;
         
-    wifiEnable = false ;
+    Serial.println("Enabling light sleep mode...") ;
+    light_sleep_button() ;
+    Serial.println("Alarm is armed.") ;
+    light_sleep_switch() ;
+    Serial.println("WARNING: ALARM HAS TRIPPED!") ;
+    
+    Serial.println("Connecting to WiFi...") ;
+    init_Wifi() ;
+    
+    Serial.println("Going to deep sleep mode...") ;
+    deep_sleep() ;
 
-    // Attach a button interrupt
-    attachInterrupt(digitalPinToInterrupt(BUTTON), ISR_buttonPressed, RISING) ;
-    // Attach a REED_PIN interrupt
-    attachInterrupt(digitalPinToInterrupt(REED_PIN), ISR_reedOpen, FALLING) ;
 
-    // Begin deep sleep mode and wake up with button press
-    deepSleep() ;
 }
 
 void loop() {
-    // If button is pressed, enable wifi
-    if (button.interruptCounter > 0) {
-        portENTER_CRITICAL(&button.mux) ;
-        button.interruptCounter-- ;
-        portEXIT_CRITICAL(&button.mux) ;
-
-        // Interrupt handling code
-        button.totalInterruptCounter++ ;
-        Serial.printf("A button interrupt has occurred. Total Number: %u\n", button.totalInterruptCounter) ;
-        if ( (button.totalInterruptCounter) && (button.totalInterruptCounter % 2) ) {   // Anything but zero and even presses
-            // Enable light sleep mode
-            lightSleep() ;
-            
-            // Enable wifi flag when exiting lightSleep mode
-            wifiEnable = true ;
-            // Make an alarm tripped flag
-            printf("WARNING: Switch has opened!\n") ;
-            ledState = LED_ON ;
-            digitalWrite(LED_PIN, ledState) ;            
-        }
-        else {
-            // Deep sleep mode if button is pressed again
-            deepSleep() ;
-        }
-        if (wifiEnable) {
-            initWifi() ;
-            wifiEnable = false ;
-        }
-        // Send an smtp email
-        delay(TRIP_DELAY)   ;
-    }
-    // while ( (button.totalInterruptCounter) && (button.totalInterruptCounter % 2) )
-    //     checkReedState() ;
-
-
-    #ifdef later
-    // Figure out a way to enable wifi once and only o
-    // Make it so that this is a wakeup from sleep
-    #endif
+    // Do nothing
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /* FUNCTION PROTOTYPES */
 
 /* Interrupt service request for button */
-void IRAM_ATTR ISR_buttonPressed(void) {
-    portENTER_CRITICAL_ISR(&button.mux) ;
-    button.interruptCounter++ ;
-    portEXIT_CRITICAL_ISR(&button.mux) ;
-}
+// void IRAM_ATTR ISR_buttonPressed(void) {
+//     portENTER_CRITICAL_ISR(&button.mux) ;
+//     button.interruptCounter++ ;
+//     portEXIT_CRITICAL_ISR(&button.mux) ;
+// }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* Interrupt service request for button */
-void IRAM_ATTR ISR_reedOpen(void) {
-    portENTER_CRITICAL_ISR(&reed.mux) ;
-    reed.interruptCounter++ ;
-    portEXIT_CRITICAL_ISR(&reed.mux) ;
-}
+// void IRAM_ATTR ISR_reedOpen(void) {
+//     portENTER_CRITICAL_ISR(&reed.mux) ;
+//     reed.interruptCounter++ ;
+//     portEXIT_CRITICAL_ISR(&reed.mux) ;
+// }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /* Reads the digital pin of the reed switch and turns an LED on if it is open */
-void checkReedState(void) {
-    // Reed_State_t    reedState ;
-    // LED_State_t     ledState ;
+// void checkReedState(void) {
+//     // Reed_State_t    reedState ;
+//     // LED_State_t     ledState ;
 
-    reedState = (Reed_State_t)( digitalRead(REED_PIN) ) ;
+//     reedState = (Reed_State_t)( digitalRead(REED_PIN) ) ;
 
-    if (reedState != REED_CLOSED) {                                             // LED turns on if reed isn't closed
-        ledState = LED_ON ;
-        digitalWrite(LED_PIN, ledState) ;
-    }
-    else {
-        ledState = LED_OFF ;
-        digitalWrite(LED_PIN, ledState) ;
-    }
+//     if (reedState != REED_CLOSED) {                                             // LED turns on if reed isn't closed
+//         ledState = LED_ON ;
+//         digitalWrite(LED_PIN, ledState) ;
+//         printf("WARNING: Door is open!") ;
+//     }
+//     else {
+//         ledState = LED_OFF ;
+//         digitalWrite(LED_PIN, ledState) ;
+//     }
 
-    delay(READ_DELAY) ;
-}
+//     delay(READ_DELAY) ;
+// }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void initWifi(void) {
+void init_Wifi(void) {
     uint8_t tries;
     // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA) ;
@@ -110,9 +78,8 @@ void initWifi(void) {
     delay(100) ;
 
     WiFi.begin(SSID, PASS) ;
-    Serial.printf("Connection to WiFi...\n") ;
     tries = 0 ;
-    while ( (WiFi.status() != WL_CONNECTED) || (++tries < 20) ) {
+    while ( (WiFi.status() != WL_CONNECTED) || (++tries < MAX_WIFI_ATTEMPTS) ) {
         Serial.printf(".") ;
         delay(1000) ;
     }
@@ -121,8 +88,8 @@ void initWifi(void) {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void deepSleep(void) {
-    esp_sleep_enable_ext0_wakeup(BUTTON, BUTTON_ON) ;                                      // Configures deep sleep wakeup sources (GPIO)
+void deep_sleep(void) {
+    esp_sleep_enable_ext0_wakeup(REED_PIN, REED_OPEN) ;                                      // Configures deep sleep wakeup sources (GPIO)
                                                                                             // then puts the ESP32 into deep sleep mode.
     esp_deep_sleep_start() ;                                                                // Prints wakeup reason when woken up.
     Serial.println("Enabling deep sleep mode...") ;
@@ -139,8 +106,25 @@ void deepSleep(void) {
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void lightSleep(void) {                                                                   // Put the device in light sleep mode.
-    Serial.println("Enabling light sleep mode...") ;
+void light_sleep_button(void) {                                                                   // Put the device in light sleep mode.
+    esp_sleep_enable_ext0_wakeup(BUTTON, BUTTON_ON) ;                                      // Configures light sleep wakeup sources (GPIO)
+                                                                                            // then puts the ESP32 into light sleep mode.
+    esp_light_sleep_start() ;                                                               // Prints wakeup reason when woken up.
+
+    wakeup_reason = esp_sleep_get_wakeup_cause() ;
+    switch(wakeup_reason)
+    {
+        case ESP_SLEEP_WAKEUP_EXT0      : Serial.println("Wakeup caused by external signal using RTC_IO") ;             break ;
+        case ESP_SLEEP_WAKEUP_EXT1      : Serial.println("Wakeup caused by external signal using RTC_CNTL") ;           break ;
+        case ESP_SLEEP_WAKEUP_TIMER     : Serial.println("Wakeup caused by timer") ;                                    break ;
+        case ESP_SLEEP_WAKEUP_TOUCHPAD  : Serial.println("Wakeup caused by touchpad") ;                                 break ;
+        case ESP_SLEEP_WAKEUP_ULP       : Serial.println("Wakeup caused by ULP program") ;                              break ;
+        default                         : Serial.printf("Wakeup was not caused by deep sleep: %d\n",wakeup_reason) ;    break ;
+    }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void light_sleep_switch(void) {                                                                   // Put the device in light sleep mode.
     esp_sleep_enable_ext0_wakeup(REED_PIN, REED_OPEN) ;                                      // Configures light sleep wakeup sources (GPIO)
                                                                                             // then puts the ESP32 into light sleep mode.
     esp_light_sleep_start() ;                                                               // Prints wakeup reason when woken up.
