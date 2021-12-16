6dfe58d7bf930b5f2086633122779df80322ac2c
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2021-12-16 15:07:26 -0600

    Refactor wifi modules to use a struct My_Wifi_t

--- homeAlarm/include/my_wifi.h
+++ homeAlarm/include/my_wifi.h
@@ -2,9 +2,10 @@
 #define MY_WIFI_H_
 
 #include <stdint.h>
-
-char const *SSID                    = "myNetworkName" ;
-char const *PASS                    = "myNetworkPassword" ;
-uint8_t const MAX_WIFI_ATTEMPTS     = 20 ;
+typedef struct my_wifi {
+    char const *SSID                    = "myNetworkName" ;
+    char const *PASS                    = "myNetworkPassword" ;
+    uint8_t const MAX_WIFI_ATTEMPTS     = 20 ;
+} My_Wifi_t ;
 
 #endif /* MY_WIFI_H_ */
--- homeAlarm/src/wifi.cpp
+++ homeAlarm/src/wifi.cpp
@@ -1,17 +1,19 @@
+#include "main.h"
 #include "my_wifi.h"
 #include <WiFi.h>
 
 /* Connects to WiFi  and shows which IP you are connected to afterwards*/
 void init_Wifi(void) {
+    My_Wifi_t wifi ;
     uint8_t tries;
     // Set WiFi to station mode and disconnect from an AP if it was previously connected
     WiFi.mode(WIFI_STA) ;
     WiFi.disconnect() ;
     delay( 100 ) ;
 
-    WiFi.begin(SSID, PASS) ;
+    WiFi.begin(wifi.SSID, wifi.PASS) ;
     tries = 0 ;
-    while ( (++tries < MAX_WIFI_ATTEMPTS) || (WiFi.status() != WL_CONNECTED) ) {
+    while ( (++tries < wifi.MAX_WIFI_ATTEMPTS) || (WiFi.status() != WL_CONNECTED) ) {
         Serial.printf(".") ;
         delay( 100 ) ;
     }
